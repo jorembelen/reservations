69ea2eb9c9c07865afa333405a4fd31f2fd1538d
jorembelen <jorembelen@gmail.com>
2023-08-24 13:55:11 +0300

    scheduled delete of older reservations added

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('reservations:delete')->weekly();
+        $schedule->command('reservations:delete')->everyMinute();
     }
 
     /**
--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -1,18 +1,18 @@
 <nav id="sidebar" class="sidebar">
     <div class="sidebar-content js-simplebar">
-        <a class="sidebar-brand" href="/dashboard">
-            <h2 class="align-middle mr-3">RCL</h2></a>
+        <a class="sidebar-brand" href="{{ route('dashboard') }}">
+            <h2 class="align-middle mr-3">Reservations</h2></a>
         <ul class="sidebar-nav">
             <li class="sidebar-header">
                 Pages
             </li>
             <li class="sidebar-item">
-                <a href="/dashboard" data-toggle="collapse" class="sidebar-link collapsed" aria-expanded="false">
+                <a href="{{ route('dashboard') }}" class="sidebar-link collapsed">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-sliders align-middle"><line x1="4" y1="21" x2="4" y2="14"></line><line x1="4" y1="10" x2="4" y2="3"></line><line x1="12" y1="21" x2="12" y2="12"></line><line x1="12" y1="8" x2="12" y2="3"></line><line x1="20" y1="21" x2="20" y2="16"></line><line x1="20" y1="12" x2="20" y2="3"></line><line x1="1" y1="14" x2="7" y2="14"></line><line x1="9" y1="8" x2="15" y2="8"></line><line x1="17" y1="16" x2="23" y2="16"></line></svg> <span class="align-middle">Dashboard</span>
                 <span class="badge badge-sidebar-primary">2</span>
                 </a>
                 <ul id="dashboard" class="sidebar-dropdown list-unstyled collapse" data-parent="#sidebar">
-                    <li class="sidebar-item"><a class="sidebar-link" href="/dashboard">Dashboard</a></li>
+                    <li class="sidebar-item"><a class="sidebar-link" href="{{ route('dashboard') }}">Dashboard</a></li>
                     
                 </ul>
             </li>
